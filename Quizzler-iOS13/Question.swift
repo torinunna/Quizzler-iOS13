b9f5e67a19ecc5cf2dbef347852a3b5c3a325503
torinunna <torinunna9588@gmail.com>
2022-03-10 00:39:17 +0900

    turn 2D quiz array into an array of quiz items created from a structure

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by 권유진 on 2022/03/10.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        ["Four + Two is equal to Six", "True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten", "False"]]
+        Question(text: "Four + Two is equal to Six", answer: "True"),
+        Question(text: "Five - Three is greater than One", answer: "True"),
+        Question(text: "Three + Eight is less than Ten", answer: "False")
+    ]
     
     var questionNumber = 0
     
@@ -31,8 +32,8 @@ class ViewController: UIViewController {
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle //True, False
-        let actualAnswer = quiz[questionNumber][1]
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
             print("Right!")
@@ -50,7 +51,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
     }
 
 
